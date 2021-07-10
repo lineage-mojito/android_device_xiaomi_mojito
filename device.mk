272f90894aa1081d30f1f59c8518d363b6b09382
AbhiShek Aggarwal <warabhishek@gmail.com>
2021-07-10 12:04:25 +0530

    mojito: Import brightness overlays from MIUI
    Change-Id: Ib00b3817eece7bb8ff0acafb3d545e4324d7c26d

--- device.mk
+++ device.mk
@@ -20,6 +20,10 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 # API level, the device has been commercially launched on
 PRODUCT_SHIPPING_API_LEVEL := 30
 
+# Overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
